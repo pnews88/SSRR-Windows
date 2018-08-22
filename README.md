a8104d83a218d46c7e9fcd949772bb68ab86bda3
CGDF <17256673+CGDF-Github@users.noreply.github.com>
2018-08-23 03:52:04 +0800

    update to 5.1.6

---
+++ README.md
@@ -0,0 +1,39 @@
+ShadowsocksR for Windows
+=======================
+
+[![Build Status]][Appveyor]
+
+
+### Environment
+
+.Net 4.5+.
+
+No \[360/2345/Baidu\], etc.
+
+\[360/2345/百度\]全家桶用户请使用[SSRR - Akkariiin](https://github.com/shadowsocksrr/shadowsocksr-csharp)
+
+### Usage
+
+Please ask the merchant.
+
+### Develop
+
+Visual Studio 2017 is recommended.
+
+### Telegram
+
+https://t.me/joinchat/GvbhT0vatPxTnqLtUgPe7Q
+
+### License
+
+GPLv3
+
+Copyright © Akkariiin 2018. Forked from ShadowsocksR by BreakWa11
+
+[Appveyor]:       https://ci.appveyor.com/project/Akkariiin/shadowsocksr-csharp
+[Build Status]:   https://ci.appveyor.com/api/projects/status/ey901turnakim5nv/branch/master?svg=true
+
+
+### Donate
+
+<img width="420" src="https://raw.githubusercontent.com/SoDa-GitHub/shadowsocksrr-csharp/master/donate.jpg"/>
---
+++ shadowsocks-csharp/View/ResetPassword.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Shadowsocks.View
+{
+    partial class ResetPassword
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.textOld = new System.Windows.Forms.TextBox();
+            this.textPassword = new System.Windows.Forms.TextBox();
+            this.textPassword2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Location = new System.Drawing.Point(16, 142);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(397, 38);
+            this.buttonOK.TabIndex = 3;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // textOld
+            // 
+            this.textOld.Location = new System.Drawing.Point(159, 34);
+            this.textOld.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textOld.Name = "textOld";
+            this.textOld.Size = new System.Drawing.Size(257, 26);
+            this.textOld.TabIndex = 0;
+            this.textOld.UseSystemPasswordChar = true;
+            // 
+            // textPassword
+            // 
+            this.textPassword.Location = new System.Drawing.Point(159, 70);
+            this.textPassword.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textPassword.Name = "textPassword";
+            this.textPassword.Size = new System.Drawing.Size(257, 26);
+            this.textPassword.TabIndex = 1;
+            this.textPassword.UseSystemPasswordChar = true;
+            // 
+            // textPassword2
+            // 
+            this.textPassword2.Location = new System.Drawing.Point(159, 106);
+            this.textPassword2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textPassword2.Name = "textPassword2";
+            this.textPassword2.Size = new System.Drawing.Size(257, 26);
+            this.textPassword2.TabIndex = 2;
+            this.textPassword2.UseSystemPasswordChar = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Old Password";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "New Password";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Confirm Password";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(343, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "This password is use to encrypt local SSR data.";
+            // 
+            // ResetPassword
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 191);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textPassword2);
+            this.Controls.Add(this.textPassword);
+            this.Controls.Add(this.textOld);
+            this.Controls.Add(this.buttonOK);
+            this.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "ResetPassword";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ResetPassword";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ResetPassword_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.TextBox textOld;
+        private System.Windows.Forms.TextBox textPassword;
+        private System.Windows.Forms.TextBox textPassword2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
